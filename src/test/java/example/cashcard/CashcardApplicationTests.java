37ef080a8b3067a37aee14a439f08b94ad8a2375
Aneesh Garg <aneeshgarg20@gmail.com>
2025-11-30 18:40:15 +0530

    Update CashCardController.java and CashCardApplicationTests.java

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,6 +23,10 @@ class CashCardApplicationTests {
 	void shouldReturnACashCardWhenDatatIsSaved() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+  		Number id = documentContext.read("$.id");
+		assertThat(id).isNotNull();
 	}
 
 }
