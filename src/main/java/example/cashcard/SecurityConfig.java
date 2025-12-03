35a0d594aea8d97aef800d365d116c02ca16e538
Aneesh Garg <aneeshgarg20@gmail.com>
2025-12-03 19:05:25 +0530

    Implementing DELETE

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -31,19 +31,24 @@ class SecurityConfig {
        return new BCryptPasswordEncoder();
    }
 
-   @Bean
-   UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
-    User.UserBuilder users = User.builder();
-    UserDetails sarah = users
-      .username("sarah1")
-      .password(passwordEncoder.encode("abc123"))
-      .roles("CARD-OWNER") 
-      .build();
-    UserDetails hankOwnsNoCards = users
-        .username("hank-owns-no-cards")
-        .password(passwordEncoder.encode("qrs456"))
-        .roles("NON-OWNER")
-        .build();
-    return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards);
-   }
+    @Bean
+    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder();
+        UserDetails sarah = users
+                .username("sarah1")
+                .password(passwordEncoder.encode("abc123"))
+                .roles("CARD-OWNER")
+                .build();
+        UserDetails hankOwnsNoCards = users
+                .username("hank-owns-no-cards")
+                .password(passwordEncoder.encode("qrs456"))
+                .roles("NON-OWNER")
+                .build();
+        UserDetails kumar = users
+                .username("kumar2")
+                .password(passwordEncoder.encode("xyz789"))
+                .roles("CARD-OWNER")
+                .build();
+        return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards, kumar);
+    }
 }
